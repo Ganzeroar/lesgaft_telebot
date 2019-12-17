9e4e655523508fe6f046b654b2eacd5b5b39995d
Дмитрий <ganzeroar@gmail.com>
2019-12-17 18:57:04 +0300

    Профиксил Schedule

--- lesgaft_schedule.py
+++ lesgaft_schedule.py
@@ -2,8 +2,11 @@ import schedule
 import time
 import site_parser
 
-schedule.every().day.at("10:00").do(site_parser.parse_and_searching_changes())
-schedule.every().day.at("20:00").do(site_parser.parse_and_searching_changes())
+def job():
+    site_parser.parse_and_searching_changes()
+
+schedule.every().day.at("10:00").do(job)
+schedule.every().day.at("20:00").do(job)
 
 while True:
     schedule.run_pending()
