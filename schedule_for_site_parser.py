908cef2e2f0b47b50aeff46c4e1a3da30507db63
Дмитрий <ganzeroar@gmail.com>
2019-12-20 00:52:31 +0300

    Изменил тайминги шедулера

--- schedule_for_site_parser.py
+++ schedule_for_site_parser.py
@@ -5,8 +5,9 @@ import site_parser
 def job():
     site_parser.parse_and_searching_changes()
 
-schedule.every().day.at("10:00").do(job)
-schedule.every().day.at("20:00").do(job)
+# -2 от Мск из-за неизменяемой таймзоры schedule 
+schedule.every().day.at("08:00").do(job)
+schedule.every().day.at("18:00").do(job)
 
 while True:
     schedule.run_pending()
