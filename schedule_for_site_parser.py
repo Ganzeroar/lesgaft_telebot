de4bb76bd5d2336093eb04d43070e07d49ad23d8
Дмитрий <ganzeroar@gmail.com>
2020-02-02 18:16:13 +0300

    изменил комментарий

--- schedule_for_site_parser.py
+++ schedule_for_site_parser.py
@@ -5,7 +5,7 @@ import site_parser
 def job():
     site_parser.parse_and_searching_changes_full_time_undergraduate()
 
-# -2 от Мск из-за неизменяемой таймзоры schedule 
+# -2 от Мск из-за неизменяемой таймзоны schedule 
 schedule.every().day.at("08:00").do(job)
 schedule.every().day.at("18:00").do(job)
 
