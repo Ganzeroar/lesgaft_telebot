b67c7338b96ad0cd75265f3ac912d235d725a89b
Dmitriy <ganzeroar@gmail.com>
2022-04-19 18:03:05 +0300

    добавил оповещения сотрудникам при неудачном парсинге

--- schedule_for_site_parser.py
+++ schedule_for_site_parser.py
@@ -8,17 +8,34 @@ import main
 def job():
     try:
         site_parser.run_undergraduate_parser()
+        main.send_custom_message_to_user(
+            206171081, 'Парсиг прошёл успешно')
     except Exception as exception:
         main.send_custom_message_to_user(
             206171081, f'ЭКСЕЛЬ ПАРСЕР УМЕР потому что {exception}')
-        print(exception)
+        main.send_custom_message_to_user(
+            1035761325, f'ЭКСЕЛЬ ПАРСЕР УМЕР потому что {exception}')
+        main.send_custom_message_to_user(
+            1197606586, f'ЭКСЕЛЬ ПАРСЕР УМЕР потому что {exception}')
 
-    # site_parser.run_all_parsers()
+def final_job():
+    try:
+        site_parser.run_undergraduate_parser()
+        main.send_custom_message_to_user(
+            206171081, 'Парсиг прошёл успешно')
+    except Exception as exception:
+        main.send_custom_message_to_user(
+            206171081, f'ЭКСЕЛЬ ПАРСЕР УМЕР потому что {exception}')
+        main.send_custom_message_to_user(
+            1035761325, f'ЭКСЕЛЬ ПАРСЕР УМЕР потому что {exception}')
+        main.send_custom_message_to_user(
+            1197606586, f'ЭКСЕЛЬ ПАРСЕР УМЕР потому что {exception}')
 
-
-# -2 от Мск из-за неизменяемой таймзоны schedule
-schedule.every().day.at("08:00").do(job)
-schedule.every().day.at("18:00").do(job)
+schedule.every().day.at("11:00").do(job)
+schedule.every().day.at("12:00").do(job)
+schedule.every().day.at("13:00").do(job)
+schedule.every().day.at("14:00").do(job)
+schedule.every().day.at("15:00").do(final_job)
 
 while True:
     schedule.run_pending()
